7ad6464e5f69af8bd25bbc0e5936b6c2a24211a2
Nicholas Knouf <nknouf@zeitkunst.org>
2014-08-20 00:07:59 -0400

    Adding necessary Vuforia code…not building at the moment

--- Art for Spooks/AFSAppDelegate.m
+++ Art for Spooks/AFSAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AFSAppDelegate.h"
+#import "ImageTargetsViewController.h"
 
 @implementation AFSAppDelegate
 
@@ -14,7 +15,9 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    //self.window.backgroundColor = [UIColor whiteColor];
+    //UIViewController *vc = [[[ImageTargetsViewController alloc] initWithNibName:@"ImageTargetsViewController" bundle:nil] autorelease];
+    //self.window.rootViewController = vc;
     [self.window makeKeyAndVisible];
     return YES;
 }
